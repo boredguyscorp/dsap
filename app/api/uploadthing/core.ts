23ff20b8343699b7419cd4a5e4d0a13698dec248
BG <oscarresonable2008@gmail.com>
2023-11-20 11:46:25 +0400

    add event in server action v3

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -4,7 +4,7 @@ import { createUploadthing, type FileRouter } from 'uploadthing/next'
 const f = createUploadthing()
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: '4MB', maxFileCount: 5 } })
+  imageUploader: f({ image: { maxFileSize: '4MB', maxFileCount: 3 } })
     .middleware(async (req) => {
       const user = await getToken(req)
 
