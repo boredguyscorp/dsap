9e6e540ffd2500664926e40df019466e0a6bb323
oscarresonable <oscarresonable2008@gmail.com>
2025-02-19 11:03:59 +0400

    added closing of registration.

--- app/(root)/(routes)/national-convention/page.tsx
+++ app/(root)/(routes)/national-convention/page.tsx
@@ -2,7 +2,15 @@ import { getChapters } from '@/actions/fetchers'
 import { NationalConventionForm } from './_components/form'
 
 export default async function NationalConventionPage() {
+  const showRegistration = process.env.NEXT_PUBLIC_SHOW_REGISTRATION === 'true'
+
   const chapters = await getChapters()
 
-  return <NationalConventionForm chapters={chapters} />
+  return showRegistration ? (
+    <NationalConventionForm chapters={chapters} />
+  ) : (
+    <div className='flex min-h-[calc(100vh-340px)] items-center justify-center'>
+      <p className='text-2xl font-bold text-red-500'>Registration Closed</p>
+    </div>
+  )
 }
