ca71c7e362a403adb2be7b976946e2925eca45f4
oscarresonable <oscarresonable2008@gmail.com>
2025-03-15 05:11:24 +0400

    updated next config for utfs domain

--- next.config.js
+++ next.config.js
@@ -13,20 +13,28 @@ const nextConfig = {
       'flag.vercel.app',
       'images.unsplash.com',
       'utfs.io'
+    ],
+    remotePatterns: [
+      { hostname: 'avatar.vercel.sh' },
+      { hostname: 'localhost' },
+      { hostname: 'dsaph.org' },
+      { hostname: 'uploadthing.com' },
+      { hostname: 'utfs.io' }
     ]
   },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ['@prisma/client', '@react-email/render']
-  },
-  images: {
-    remotePatterns: [
-      { hostname: 'avatar.vercel.sh' },
-      { hostname: 'localhost' },
-      { hostname: 'dsaph.org' },
-      { hostname: 'uploadthing.com' }
-    ]
   }
+  // images: {
+  //   remotePatterns: [
+  // { hostname: 'avatar.vercel.sh' },
+  // { hostname: 'localhost' },
+  // { hostname: 'dsaph.org' },
+  // { hostname: 'uploadthing.com' },
+  // { hostname: 'utfs.io' }
+  //   ]
+  // }
   // reactStrictMode: true,
   // pageExtensions: ['ts', 'tsx', 'mdx']
 }
